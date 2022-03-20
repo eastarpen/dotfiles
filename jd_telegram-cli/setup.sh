be04a00cd87d7daab95ab866e9098bf19bc69280
Eastarpen <eaststarpen@gmail.com>
2022-03-20 15:36:45 +0800

    update

--- jd_telegram-cli/setup.sh
+++ jd_telegram-cli/setup.sh
@@ -1,17 +1,19 @@
 #! /bin/bash
 
-sudo snap install  -y telegram-cli
+path=$(cd $(dirname $0);pwd)
+
+sudo snap install telegram-cli
 
 sudo mkdir -p /root/script/
 
-sudo mv ./script/jd_ShareCode.sh /root/script/
+sudo mv ${path}/script/jd_ShareCode.sh /root/script/
 
 # set time zone
-#tzselect 
+#tzselect
 #sudo cp /usr/share/zoneinfo/Asia/Shanghai  /etc/localtime
 sudo timedatectl set-timezone Asia/Shanghai
 
-# start systemd 
-sudo cp ./jd_ShareCode.service /usr/lib/systemd/system/
-sudo cp ./jd_ShareCode.timer /usr/lib/systemd/system/
+# start systemd
+sudo cp ${path}/lib/jd_ShareCode.service /usr/lib/systemd/system/
+sudo cp ${path}/lib/jd_ShareCode.timer /usr/lib/systemd/system/
 sudo systemctl start jd_ShareCode.timer
