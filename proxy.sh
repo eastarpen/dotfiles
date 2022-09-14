0858e8955da7502928e09fc94b7445abe7c59a33
Eastarpen <eaststarpen@gmail.com>
2022-09-14 15:29:57 +0800

    add proxy

---
+++ proxy.sh
@@ -0,0 +1,28 @@
+# add these script fragments to your .bashrc or .zshrc or other shell profile
+
+# check whether this system is wsl2
+if uname -r |grep -iq "WSL2";
+then
+    export winip=`ipconfig.exe | grep 'Wireless LAN adapter Wi-Fi' -A6 | cut -d":" -f 2| tail -3| head -1| sed -e 's/\s*//g'`
+fi
+
+# proxy settings
+function proxy_on() {
+    # clash port
+    address="http://$winip:7890"
+    export http_proxy=$address
+    export https_proxy=$address
+    git config --global http.proxy $address
+    git config --global https.proxy $address
+    echo -e "proxy address $address"
+}
+
+function proxy_off(){
+    unset http_proxy https_proxy
+    git config --global --unset http.proxy
+    git config --global --unset https.proxy
+    echo -e 'proxy unset!'
+}
+
+# open proxy_on defaulted
+proxy_on()
